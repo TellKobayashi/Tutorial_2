737002a0b1ef99a09a27507d191bd0a80a7d0836
TellKobayashi <haruki.kobayashi.tf@ambl.co.jp>
2022-08-04 03:00:11 +0000

    Add to microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :marimos
-  resources :users
+  resources :users#ここの書き方は、Ruby特有のシンボルと呼ばれる書き方。8080/usersと言うことrails 
   root 'application#hello'
 end
